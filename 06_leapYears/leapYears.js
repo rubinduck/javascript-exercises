9515cbbadd30c0bb97e81fb845fc4e9123ef65c1
rubinduck <rubinduck@protonmail.com>
2021-11-12 22:07:01 +0100

    Add 06 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return isDevisableBy(year, 4) && (!isDevisableBy(year, 100) || isDevisableBy(year, 400));
 };
+
+function isDevisableBy(number, deviser){
+    return number % deviser === 0;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
