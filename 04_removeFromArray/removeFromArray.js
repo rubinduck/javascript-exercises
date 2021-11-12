01274f5f2a547b290440876ba24b8ae233bb31d4
rubinduck <rubinduck@protonmail.com>
2021-11-12 21:42:25 +0100

    Add 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (let i = 0; i < elementsToRemove.length; i++){
+        const element = elementsToRemove[i];
+        let elementIndex = array.lastIndexOf(element);
+        while (elementIndex !== -1){
+            array.splice(elementIndex, 1);
+            elementIndex = array.lastIndexOf(element);
+        }
+    }
+    return array;
+    // more advanced solution
+    // return array.filter(element => !elementsToRemove.includes(element));
 };
 
 // Do not edit below this line
