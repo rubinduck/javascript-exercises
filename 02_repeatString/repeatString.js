09b06c996d29bda96c8ba8fccd1fc06c1646eef0
rubinduck <rubinduck@protonmail.com>
2021-11-12 21:27:06 +0100

    Add 02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, repeatAmount) {
+    if (repeatAmount < 0) return 'ERROR';
+    let result = '';
+    for (let i = 0; i < repeatAmount; i++)
+        result += string;
+    return result;
 
 };
 
