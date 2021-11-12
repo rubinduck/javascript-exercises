acda6b6f2b78860abf175c72e2ad216c4e97ff08
rubinduck <rubinduck@protonmail.com>
2021-11-12 22:00:04 +0100

    Add 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!isNumber(a) || !isNumber(b)) return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
+    const from = Math.min(a, b);
+    const to = Math.max(a, b);
+    let sum = 0;
+    for (let n = from; n <= to; n++)
+        sum += n;
+    return sum;
 };
+
+function isNumber(value){
+    return typeof value === 'number';
+}
 
 // Do not edit below this line
 module.exports = sumAll;
