ab4cdb88a07219eca02ebe9654d9532b7d00770f
rubinduck <rubinduck@protonmail.com>
2021-11-17 01:18:39 +0100

    Add 08 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = (numbers) => numbers.reduce((a, b) => a + b, 0);
+
+const multiply = (numbers) => numbers.reduce((a, b) => a * b, 1);
+
+const power = (number, power) => {
+  if (power === 0)
+    return 1;
+  if (power < 0){
+    number = 1/number;
+    power = -power;
+  }
+  let result = 1;
+  for (let i = 1; i <= power; i++)
+    result *= number;
+  return result
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 1; i <= n; i++)
+    result *= i;
+  return result;
 };
 
 // Do not edit below this line
