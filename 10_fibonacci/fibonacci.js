0dc63c14cc13deb2fcf5d8f0109c9d4f19edd0d6
rubinduck <rubinduck@protonmail.com>
2021-11-28 22:06:28 +0100

    Add 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = (n) => {
+    if (typeof(n) === 'string')
+        n = Number.parseInt(n);
+    if (n < 0)
+        return 'OOPS';
+    n--;
+    return fib(n);
 
-};
+    function fib(n){
+        if (n === 0 || n == 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
