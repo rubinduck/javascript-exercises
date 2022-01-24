ed898c767e183a6f69266b6a945762ef5ed23c2a
rubinduck <rubinduck@protonmail.com>
2022-01-24 22:32:29 +0100

    Add 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+    return maxBy(people, getAge)
+}
 
-};
+const getAge = (human) => {
+    const yearOfDeath = human.yearOfDeath ?? getCurrentYear();
+    return yearOfDeath - human.yearOfBirth;
+
+    function getCurrentYear(){
+        return new Date().getFullYear();
+    }
+}
+
+const maxBy = (array, f) => {
+    if (array.length === 0)
+        throw new Error("Max by called on empty array");
+    return array.reduce((a, b) => f(a) > f(b) ? a : b);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
