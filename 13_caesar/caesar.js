5286acb38c0cee72bda58990b900334592547c81
rubinduck <rubinduck@protonmail.com>
2022-01-25 00:33:21 +0100

    Add 13 solution

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,36 @@
-const caesar = function() {
-
+const caesar = (string, shift) => {
+    let resultString = '';
+    for (const char of string){
+        if (isPunctuationChar(char)){
+            resultString += char;
+            continue;
+        }
+        resultString += shiftChar(char, shift);
+    }
+    return resultString;
 };
 
-// Do not edit below this line
+const punctuationChars = new Set(['.', ',', ';', ':', '?', '!', ' ']);
+const isPunctuationChar = (char) =>
+    punctuationChars.has(char);
+
+const asciiLettersLowercase = 'abcdefghijklmnopqrstuvwxyz';
+const asciiLettersUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const alphabetLenght = 26
+const shiftChar = (char, shift) => {
+    const alphabet = isLowerCaseLetter(char) 
+        ? asciiLettersLowercase
+        : asciiLettersUppercase;
+    const charIndex = alphabet.indexOf(char);
+    let newCharIndex = charIndex + shift;
+    newCharIndex %= alphabetLenght;
+    if (newCharIndex < 0)
+        newCharIndex = alphabetLenght + newCharIndex;
+    return alphabet.charAt(newCharIndex);
+
+    function isLowerCaseLetter(char){
+        return char >= 'a' && char <= 'z';
+    }
+}
+
 module.exports = caesar;
